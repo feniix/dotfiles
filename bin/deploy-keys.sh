2dea080ee39b2f160d249f8eb41326f1c2cf6630
Sebastian Otaegui <feniix@gmail.com>
2015-02-06 11:16:29 -0600

    update plugins

--- bin/deploy-keys.sh
+++ bin/deploy-keys.sh
@@ -1,8 +1,8 @@
 keypair=$USER
 publickeyfile=$HOME/.ssh/id_rsa.pub
-regions=$(ec2-describe-regions -O $AWS_ACCESS_KEY -W $AWS_SECRET_ACCESS | cut -f2)
+regions=$(ec2-describe-regions -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY | cut -f2)
 
 for region in $regions; do
   echo $region
-  ec2-import-keypair -O $AWS_ACCESS_KEY -W $AWS_SECRET_ACCESS --region $region --public-key-file $publickeyfile $keypair
+  ec2-import-keypair -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY --region $region --public-key-file $publickeyfile $keypair
 done
