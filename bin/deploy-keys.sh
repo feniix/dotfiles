69f8a508a4561061eb634a7acf114a4955df5b49
Sebastian Otaegui <feniix@gmail.com>
2015-04-30 01:39:25 -0500

    Improve aws key deployer

--- bin/deploy-keys.sh
+++ bin/deploy-keys.sh
@@ -1,8 +1,15 @@
-keypair=$USER
-publickeyfile=$HOME/.ssh/id_rsa.pub
-regions=$(ec2-describe-regions -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY | cut -f2)
+#!/usr/bin/env bash
 
-for region in $regions; do
-  echo $region
-  ec2-import-keypair -O $AWS_ACCESS_KEY -W $AWS_SECRET_KEY --region $region --public-key-file $publickeyfile $keypair
+KEYPAIR_NAME=${KEYPAIR_NAME:=${USER}}
+keypair="${KEYPAIR_NAME}"
+if [ "${KEYPAIR_NAME}" = "${USER}" ]; then
+  publickeyfile="${HOME}/.ssh/id_rsa.pub"
+else
+  publickeyfile="${HOME}/.ssh/${KEYPAIR_NAME}.pub"
+fi
+regions=$(ec2-describe-regions -O "${AWS_ACCESS_KEY}" -W "${AWS_SECRET_KEY}" | cut -f2)
+
+for region in ${regions}; do
+  echo "${region}"
+  ec2-import-keypair -O "${AWS_ACCESS_KEY}" -W "${AWS_SECRET_KEY}" --region "${region}" --public-key-file "${publickeyfile}" "${keypair}"
 done
