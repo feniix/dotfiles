1e381a2d75b728364b9af9e844df8ec674b61225
Sebastian Otaegui <feniix@gmail.com>
2025-05-24 01:29:16 -0300

    move lang specific

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,26 +11,3 @@ require('core').setup()
 
 -- Load plugin management
 require('plugins').setup()
-
--- Additional post-plugin setup
--- Configure Go globals
-vim.g.go_fmt_command = 'goimports'
-vim.g.go_list_type = 'quickfix'
-vim.g.go_test_timeout = '10s'
-vim.g.go_highlight_types = 1
-vim.g.go_highlight_fields = 1
-vim.g.go_highlight_functions = 1
-vim.g.go_highlight_methods = 1
-vim.g.go_highlight_operators = 1
-vim.g.go_highlight_build_constraints = 1
-vim.g.go_highlight_function_calls = 1
-vim.g.go_highlight_extra_types = 1
-vim.g.go_highlight_generate_tags = 1
-vim.g.go_gopls_enabled = 1
-vim.g.go_code_completion_enabled = 1
-vim.g.go_doc_keywordprg_enabled = 1
-vim.g.go_mod_fmt_autosave = 1
-vim.g.go_fmt_autosave = 1
-vim.g.go_imports_autosave = 1
-vim.g.go_diagnostics_enabled = 1
-vim.g.go_metalinter_enabled = 1 
--- nvim/lua/plugins/config/lang/go.lua
+++ nvim/lua/plugins/config/lang/go.lua
@@ -88,6 +88,35 @@ end
 function M.setup()
   vim.notify("Using vim-go for Go development (LSP disabled)", vim.log.levels.INFO)
   
+  -- Configure vim-go plugin settings
+  -- These settings were moved from init.lua to keep language-specific configs organized
+  
+  -- Formatting and imports
+  vim.g.go_fmt_command = 'goimports'  -- Use goimports for formatting
+  vim.g.go_fmt_autosave = 1           -- Auto-format on save
+  vim.g.go_imports_autosave = 1       -- Auto-organize imports on save
+  vim.g.go_mod_fmt_autosave = 1       -- Auto-format go.mod files
+  
+  -- Tools and features
+  vim.g.go_list_type = 'quickfix'     -- Use quickfix for errors
+  vim.g.go_test_timeout = '10s'       -- Test timeout
+  vim.g.go_gopls_enabled = 1          -- Enable gopls
+  vim.g.go_code_completion_enabled = 1 -- Enable code completion
+  vim.g.go_doc_keywordprg_enabled = 1  -- Enable doc on K
+  vim.g.go_diagnostics_enabled = 1     -- Enable diagnostics
+  vim.g.go_metalinter_enabled = 1      -- Enable metalinter
+  
+  -- Syntax highlighting
+  vim.g.go_highlight_types = 1
+  vim.g.go_highlight_fields = 1
+  vim.g.go_highlight_functions = 1
+  vim.g.go_highlight_methods = 1
+  vim.g.go_highlight_operators = 1
+  vim.g.go_highlight_build_constraints = 1
+  vim.g.go_highlight_function_calls = 1
+  vim.g.go_highlight_extra_types = 1
+  vim.g.go_highlight_generate_tags = 1
+  
   -- Set up autocommand to set keymaps when Go files are opened
   vim.api.nvim_create_autocmd('FileType', {
     pattern = 'go',
